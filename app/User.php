570b0b430d25877fb059e453afb683b23426d442
Chris Mitchell <cbm3384@gmail.com>
2017-06-13 11:56:45 -0600

    Fixes notificationsEnabled method logic.

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
 
     public function notificationsEnabled()
     {
-        return $this->via_email || $this.via_slack;
+        return $this->via_email || $this->via_slack;
     }
 
     public function routeNotificationForSlack()
